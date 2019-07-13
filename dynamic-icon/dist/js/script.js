aba1650912d456be9b38bb0b07b516e3f0576416
Thiago S. M <tsm_sp@hotmail.com>
2019-07-13 16:26:06 -0300

    Dinamic DIV

---
+++ dynamic-icon/dist/js/script.js
@@ -0,0 +1,9 @@
+
+
+  var menuIco = "<div><ul>";
+    for(var i=1; i<=10; i++){
+      menuIco+= "<li class=myDivs id='ico_menu"+i+"'></li>";
+    }
+  menuIco+= '</ul></div>';
+  $('#container').append(menuIco);
+  
